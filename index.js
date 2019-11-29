958c9e0b9754aa3e11c518bdd870fcab20139e35
kewang <cpckewang@gmail.com>
2019-11-29 21:46:26 +0800

    Specify Chrome location

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const prerender = require("prerender");
-const server = prerender();
+const prerender = require('prerender');
+const server = prerender({
+  chromeLocation: '/app/.apt/usr/bin/google-chrome'
+});
 
 server.start();
