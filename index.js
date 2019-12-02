37e70467994c78be8e50ab2da154a1a97d21ce21
kewang <cpckewang@gmail.com>
2019-12-02 10:20:27 +0800

    Add prerender memory cache

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const prerender = require('prerender');
+const memoryCache = require('prerender-memory-cache');
+
 const server = prerender({
   chromeLocation: '/app/.apt/usr/bin/google-chrome'
 });
+
+server.use(memoryCache);
 
 server.start();
