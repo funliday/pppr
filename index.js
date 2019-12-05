030b62c6ded603de34c9d02ca0b9a574b69c1a78
kewang <cpckewang@gmail.com>
2019-12-05 19:05:41 +0800

    Show user agent

--- index.js
+++ index.js
@@ -5,6 +5,21 @@ const server = prerender({
   chromeLocation: '/app/.apt/usr/bin/google-chrome'
 });
 
+const ShowUserAgent = {
+  init: () => {},
+
+  beforeSend: (req, res, next) => {
+    console.log(
+      `${new Date()} user-agent: ${req.headers['user-agent']} for ${
+        req.prerender.url
+      }`
+    );
+
+    next();
+  }
+};
+
+server.use(ShowUserAgent);
 server.use(memoryCache);
 
 server.start();
