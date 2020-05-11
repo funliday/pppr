9db0d75ef61d71a8a8de726243616afed4644c0f
kewang <cpckewang@gmail.com>
2020-05-11 11:57:55 +0800

    Add PRERENDER_WAIT_AFTER_LAST_REQUEST

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const prerender = require('prerender');
 const memoryCache = require('prerender-memory-cache');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const server = prerender({
-  chromeLocation: '/app/.apt/usr/bin/google-chrome'
+  chromeLocation: '/app/.apt/usr/bin/google-chrome',
+  waitAfterLastRequest: +process.env.PRERENDER_WAIT_AFTER_LAST_REQUEST
 });
 
 const ShowUserAgent = {
